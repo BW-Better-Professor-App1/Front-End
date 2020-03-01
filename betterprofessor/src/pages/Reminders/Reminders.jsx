2eb8e78d7c20c3e2177c2e1c97f02b2d16fa4a47
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-01 16:26:43 -0600

    added basic structure for the app.

---
+++ betterprofessor/src/pages/Reminders/Reminders.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Reminders = () => {
+  return (
+    <div>
+      <h1>Add Reminders</h1>
+      <div className="container">
+        <div>
+          <h1>Reminder Form</h1>
+          <h1>Reminder List</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reminders;

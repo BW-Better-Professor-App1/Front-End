69ac308d6a5d62f10a4265c7e67fdfa7303a3fb8
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-01 16:07:42 -0600

    added remider to the store.

--- betterprofessor/src/redux/root-reducer.js
+++ betterprofessor/src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
+import reminderReducer from "./reminder/reminder.reducer";
 
 export const rootReducer = combineReducers({
-  userReducer
+  userReducer,
+  reminderReducer
 });

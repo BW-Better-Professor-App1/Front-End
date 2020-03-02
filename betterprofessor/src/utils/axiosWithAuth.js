4239eb46d683bd39c677da62562e2105301b4b4c
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-01 22:32:43 -0600

    changed to session storage for better security and inserted backend server link.

--- betterprofessor/src/utils/axiosWithAuth.js
+++ betterprofessor/src/utils/axiosWithAuth.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem("token");
-
+  const token = sessionStorage.getItem("token");
   return axios.create({
+    baseURL: "https://best-professor.herokuapp.com/api",
     headers: {
       Authorization: token
     }

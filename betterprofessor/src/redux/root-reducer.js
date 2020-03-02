0691e452071ba7280b0f9b790b615118afc3c3cd
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-01 22:33:58 -0600

    decided not to use redux on user creation and login.

--- betterprofessor/src/redux/root-reducer.js
+++ betterprofessor/src/redux/root-reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 
-import userReducer from "./user/user.reducer";
 import reminderReducer from "./reminder/reminder.reducer";
 
 export const rootReducer = combineReducers({
-  userReducer,
   reminderReducer
 });
